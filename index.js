cdcaa7f10f5e63376634c1a5862a16690495509a
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2018-08-26 20:49:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const env = nunjucks.configure(['web'], {
     express: app
 });
 
+let data = [{
+    name: 'Local',
+    gamemode: 'FFA',
+    address: '127.0.0.1',
+    protocol: '18'
+}]; // todo database
+
 app.get('/', (req, res) => {
-    res.render('index.njk');
+    res.render('index.njk', data);
 });
 
 app.post('/addserver', (req, res) => {
