e5fac4ec02a603bf0404879ce4d7a8535ac76a26
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2018-08-26 15:31:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express'), app = express();
 
+const env = nunjucks.configure(['web'], {
+    autoescape: true,
+    express: app
+});
+
 app.use(express.static('web'));
 
 app.post('/addserver', (req, res) => {
