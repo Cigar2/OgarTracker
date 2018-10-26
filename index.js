7364f9fdbecdb18cbe4280714b3b3649ebc745c9
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2018-10-26 09:45:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ let data = [{
 
 const startupDate = Date.now();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.render('index.njk', {
         servers: data,
@@ -23,16 +25,14 @@ app.get('/', (req, res) => {
     });
 });
 
-app.use(express.json());
+app.get('/json', (req, res) => res.json(data));
 
 app.post('/updateserver', (req, res) => {
     const { name, gamemode, address, onlineSince, operatingSystem, protocol } = req.body;
     if(typeof name !== 'string' || typeof gamemode !== 'string' || typeof address !== 'string' ||
        typeof onlineSince !== 'number' || typeof operatingSystem !== 'string' || typeof protocol !== 'string')
         return res.json({success: false, error: 'Incomplete/invalid request.'});
-    const obj = {
-        name, gamemode, address, onlineSince, operatingSystem, protocol
-    };
+    const obj = { name, gamemode, address, onlineSince, operatingSystem, protocol };
     data.push(obj);
 });
 
